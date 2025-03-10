c85a0f36b96e59cf49174b4512a7d600131218e2
TopScrech <sergei_saliukov@icloud.com>
2025-03-10 13:56:45 +0100

    Widget setup guide

--- Widgets/Views/Small Widget View.swift
+++ Widgets/Views/Small Widget View.swift
@@ -20,7 +20,7 @@ struct BaseWidgetView: View {
     }
     
     var body: some View {
-        ZStack {            
+        ZStack {
             VStack(alignment: .trailing, spacing: 0) {
                 Text(entry.vm.serverName)
                     .semibold()
@@ -131,25 +131,7 @@ struct SmallWidgetView: View {
     
     var body: some View {
         if entry.vm.viewType == .Unconfigured {
-            VStack(spacing: 10) {
-                Label {
-                    Text("MC Stats")
-                        .bold()
-                } icon: {
-                    Image(.icon)
-                        .resizable()
-                        .frame(width: 32, height: 32)
-                        .clipShape(.rect(cornerRadius: 8))
-                }
-                
-                VStack(alignment: .leading, spacing: 5) {
-                    Text("1. **Long press** the widget")
-                    Text("2. Tap on **Edit Widget**")
-                    Text("3. **Choose your server** from the list")
-                }
-                .fontSize(10)
-            }
-            .padding()
+            ConfigureWidgetView()
         } else {
             if entry.configuration.Theme == nil || entry.configuration.Theme?.id ?? "" == Theme.auto.rawValue {
                 BaseWidgetView(entry)
